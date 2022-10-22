04bbcd310fce89f8377a723ed56a85cf435be21c
Cristian Machado <56597872+cmachado17@users.noreply.github.com>
2022-10-22 15:40:29 -0300

    dominio + referencia a tpc

---
+++ TPC-Leiva-Machado/Dominio/Incidente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Incidente
+    {
+        public int Id { get; set; }
+        public TipoIncidencia Tipo { get; set; }
+        public Prioridad Prioridad { get; set; }
+        public string Problematica { get; set; }
+        public Estado Estado { get; set; }
+        public Cliente Cliente { get; set; }
+        public string Resolucion { get; set; }
+        public Telefonista Telefonista { get; set; }
+        public string Motivo { get; set; }
+    }
+}
